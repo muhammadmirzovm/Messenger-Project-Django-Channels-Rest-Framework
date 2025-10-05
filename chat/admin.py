06198143f4d83fb14056e8bead481a6f5853161c
muhammadmirzo <mukhammadmirzo078@gmail.com>
2025-10-05 14:52:05 +0500

    Added gitignore

--- chat/admin.py
+++ chat/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import User, Room, RoomMembership, Message
+
 admin.site.register(User)
 admin.site.register(Room)
 admin.site.register(RoomMembership)
